ed773a394bad0a93969d7db44f7147dba0610737
Siamashka Vlad <wladsemashko@gmail.com>
2019-01-29 09:45:30 +0100

    q

--- stack.cpp
+++ stack.cpp
@@ -1,40 +1,44 @@
 #include <stdlib.h>
 #include "stack.h"
 
+typedef struct NodeImplementation* Node;
 
-struct Node {
-    struct Node* next;
+struct NodeImplementation {
+    Node next;
     void* data;
 };
 
 struct StackImplementation {
-    struct Node* head;
+    Node head;
     int count;
 };
 
 Stack create_stack() {
-    Stack stack = (Stack) smalloc(sizeof(StackImplementation));
-    stack->head = 0;
-    stack->count = 0;
-    return stack;
+  Stack stack = (Stack)smalloc(sizeof(StackImplementation));
+  stack->head=0;
+  stack->count=0;
+  return stack;
 }
 
 void delete_stack(Stack stack) {
-    struct Node* node = stack->head;
-    while (node != 0) {
-        Node *tmp = node;
-        node = node->next;
-        sfree(tmp);
-    }
-    sfree(stack);
+  Node current=stack->head;
+  Node tmp;
+  while (current!=0) {
+    tmp=current;
+    current=current->next;
+    sfree(tmp);
+  }
+  sfree(stack);
+
 }
 
 void push_stack(Stack stack, void *data) {
-    Node* new_node = (Node*) smalloc(sizeof(Node));
-    new_node->data = data;
-    new_node->next = stack->head;
-    stack->head = new_node;
-    stack->count++;
+  Node new_node=(Node)smalloc(sizeof(NodeImplementation));
+  new_node->data=data;
+  new_node->next=stack->head;
+  stack->head=new_node;
+  stack->count++;
+
 }
 
 int get_count(Stack stack) {
@@ -42,20 +46,12 @@ int get_count(Stack stack) {
 }
 
 void* pop_stack(Stack stack) {
-    if (get_count(stack) == 0){
-        return 0;
-    }
-    Node* top = stack->head;
-    stack->head = top->next;
-    stack->count--;
-    void* data = top->data;
-    sfree(top);
-    return data;
+  //if (get_count(stack)==0) {
+    //return 0;
+  //}
+  
 }
 
 void* peek_stack(Stack stack) {
-    if (get_count(stack) == 0){
-        return 0;
-    }
-    return stack->head->data;
+
 }
